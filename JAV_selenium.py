0b8c00b38f59f8ac9bee89396ca86fa29c1cd6cc
Connor Kenway <nightmoon0731@gmail.com>
2023-09-09 16:58:37 -0700

    feat: able to download the music

--- JAV_selenium.py
+++ JAV_selenium.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import requests
 # import Action chains 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -10,7 +11,7 @@ driver = webdriver.Chrome()
 driver.get("https://javmodel.com/jav/homepages.php?")
 driver.maximize_window()
 
-element = driver.find_element("link text", "Akiho Yoshizawa")
+element = driver.find_element("element", "img src")
 driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
 time.sleep(3)
@@ -31,3 +32,24 @@ current_url = driver.current_url
 
 # Print or return the URL
 print("The current URL is:", current_url)
+
+# Assuming the image has an id attribute "someImageId"
+img_element = driver.find_element(By.CLASS_NAME, "veryclear")
+
+# Get the image source URL
+img_url = img_element.get_attribute('src')
+
+# Download the image
+response = requests.get(img_url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Save the image
+    with open('image.jpg', 'wb') as f:
+        f.write(response.content)
+    print("Image downloaded successfully.")
+else:
+    print("Failed to download the image.")
+
+# Close the driver
+driver.quit()
