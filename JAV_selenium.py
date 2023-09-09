23fb20a3832669fdb5fbd3d15643b0787bf545f2
Connor Kenway <nightmoon0731@gmail.com>
2023-09-09 16:50:12 -0700

    feat: able to automaticall scroll and add the page

--- JAV_selenium.py
+++ JAV_selenium.py
@@ -10,10 +10,10 @@ driver = webdriver.Chrome()
 driver.get("https://javmodel.com/jav/homepages.php?")
 driver.maximize_window()
 
-time.sleep(3)
-
 element = driver.find_element("link text", "Akiho Yoshizawa")
 driver.execute_script("arguments[0].scrollIntoView(true);", element)
+
+time.sleep(3)
 
 # create action chain object    
 action = ActionChains(driver)
@@ -21,11 +21,13 @@ action = ActionChains(driver)
 # click the item
 action.click(on_element = element)
 
-# Capture the current URL
-current_url = driver.current_url
-
 # perform the operation
 action.perform()
+
+time.sleep(5)
+
+# Capture the current URL
+current_url = driver.current_url
 
 # Print or return the URL
 print("The current URL is:", current_url)
